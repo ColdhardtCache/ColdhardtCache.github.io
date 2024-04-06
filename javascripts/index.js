8495f946bd367e72668994a4a18f4bbc89fdc79f
Brian <brian.grier16@gmail.com>
2024-04-06 00:24:28 -0400

    starting from stratch

---
+++ javascripts/index.js
@@ -0,0 +1,7 @@
+window.addEventListener("load", function(event) {
+    document.getElementById("bigButtonToProj").addEventListener("click", goToProj);
+});
+
+function goToProj() {
+    window.location.href = "/project.html"
+}
